a9b2bad26b9c837e8f2b0e4ec8852968f45f843a
hopeyuniawasonga <hope.wasonga@student.moringaschool.com>
2025-04-19 13:28:13 +0300

    jsx lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return(
+  <div id="about">
+      <h2>About Me</h2>
+      <p>I am passionate about building user-friendly web applications that make a difference.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
